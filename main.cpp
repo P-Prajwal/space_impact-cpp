d2f2df786865cb73da3170ce54af56fcea08ad85
P-Prajwal <58064369+P-Prajwal@users.noreply.github.com>
2022-02-25 16:21:36 +0530

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,47 +1,39 @@
 #include<iostream>
-#include "spaceShip.h"
+#include "spaceship.h"
 #include "enemy.h"
 #include "screen.h"
 #include "goto.h"
 using namespace std;
-
 int main(){
+        int life=3;
+        gamePlayFrame();
+        starFighter spaceShip(5,15);
+        spaceShip.paint();//Start painting ship
+          bool gameOver=false;
+ while(gameOver!=true){
+                spaceShip.move();
 
-	windowAdjust(122,133);
-
-	spaceship.paint();//Start painting ship
-
-	while(gameOver!=true){
-		gamePlayerFrame();
-		spaceship.move();
-		
-		//Next bullet functions and enemy formation to be added, working on them.
-		
+                //Next bullet functions and enemy formation to be added, working on them.
 
 
-	}
+
+        }
 
 
-	if(gameOver==true){
-		//if we are out of life, then game over
-		if(life<=0){
-			gotoxy(56,15);
-			cout<<"GAME OVER";
-		}
+        if(gameOver==true){
+                //if we are out of life, then game over
+                if(life<=0){
+                        gotoxy(56,15);
+                        cout<<"GAME OVER";
+                }
 
-	 else{ //if we reach here means we are alive and the game has finished
-                
-		 gotoxy(56,15);
-		 cout<<"VICTORY";
-	 }
+         else{ //if we reach here means we are alive and the game has finished
 
-	}
+                 gotoxy(56,15);
+                 cout<<"VICTORY";
+         }
+ }
 
-	getch();
-	return 0;
+        return 0;
 }
-	     
-
- 
-		
 
